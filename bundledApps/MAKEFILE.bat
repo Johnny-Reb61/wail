90aa36f78bb0a1d41b21c1cf1fe219298b795e05
Mat Kelly <me@matkelly.com>
2018-01-22 15:33:08 -0800

    Update slashes for windows 'MAKEFILE'

--- bundledApps/MAKEFILE.bat
+++ bundledApps/MAKEFILE.bat
@@ -1,7 +1,7 @@
-pyinstaller --onefile --windowed --version-file=build/version.txt --icon=build/icons/wail_blue.ico bundledApps/WAIL.py
+pyinstaller --onefile --windowed --version-file=build\version.txt --icon=build\icons\wail_blue.ico bundledApps\WAIL.py
 
 ::Windows shell (CMD.exe)
 move /Y ".\dist\WAIL.exe" ".\WAIL.exe"
 
 ::Unix shell (e.g., from Git Bash on Windows)
-::mv "./dist/WAIL.exe" "./WAIL.exe" 
+::mv ".\dist\WAIL.exe" ".\WAIL.exe" 
